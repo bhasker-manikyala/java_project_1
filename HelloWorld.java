445c23b6d6e65e11d5e613227db20880fda01257
bhasker-manikyala <28338138+bhasker-manikyala@users.noreply.github.com>
2025-09-19 21:31:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Jenkins Online Training!");
+        System.out.println("Welcome to Jenkins and Git Online Training!");
 
     }
 
